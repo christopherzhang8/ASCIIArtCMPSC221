5df65b92706ea62207744b689fdd5b877c0b98c4
christopherzhang8 <42556126+christopherzhang8@users.noreply.github.com>
2018-08-26 17:29:27 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,9 +3,11 @@ public class Main
   
    public static void main(String[] args) 
    {
-      System.out.println("  *  ");
-      System.out.println(" *** ");
-      System.out.println("*****");
+      System.out.println(" *   * ***** *     *     *****");
+      System.out.println(" *   * *     *     *     *   *");
+      System.out.println(" ***** ***** *     *     *   *");
+      System.out.println(" *   * *     *     *     *   *");
+      System.out.println(" *   * ***** ***** ***** *****");
   }
   
 }
